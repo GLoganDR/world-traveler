2a95d5ac52def29f65433f1c15a592e614a003c2
Logan Richardson <g.logan.richardson@gmail.com>
2014-08-18 17:50:19 -0500

    Edited and adding pushpins

--- app/static/js/user/vacations.js
+++ app/static/js/user/vacations.js
@@ -7,10 +7,24 @@
 
   $(document).ready(function(){
     initMap(0, 0, 2);
+    var positions = getPositions();
+    console.log(positions);
   });
 
+  function getPositions(){
+    var positions = $('table tbody tr').map(function(tr){
+      var name = $(tr).attr('data-name'),
+          lat  = $(tr).attr('data-lat'),
+          lng  = $(tr).attr('data-lng'),
+          pos  = {name:name, lat:parseFloat(lat), lng:parseFloat(lng)};
+      return pos;
+    });
+    return positions;
+  }
+
   function initMap(lat, lng, zoom){
-    var mapOptions = {center: new google.maps.LatLng(lat, lng), zoom: zoom, mapTypeId: google.maps.MapTypeId.ROADMAP};
-    map        = new google.maps.Map(document.getElementById('map'), mapOptions);
+    var styles     = [{'featureType':'water','elementType':'geometry','stylers':[{'color':'#ffdfa6'}]},{'featureType':'landscape','elementType':'geometry','stylers':[{'color':'#b52127'}]},{'featureType':'poi','elementType':'geometry','stylers':[{'color':'#c5531b'}]},{'featureType':'road.highway','elementType':'geometry.fill','stylers':[{'color':'#74001b'},{'lightness':-10}]},{'featureType':'road.highway','elementType':'geometry.stroke','stylers':[{'color':'#da3c3c'}]},{'featureType':'road.arterial','elementType':'geometry.fill','stylers':[{'color':'#74001b'}]},{'featureType':'road.arterial','elementType':'geometry.stroke','stylers':[{'color':'#da3c3c'}]},{'featureType':'road.local','elementType':'geometry.fill','stylers':[{'color':'#990c19'}]},{'elementType':'labels.text.fill','stylers':[{'color':'#ffffff'}]},{'elementType':'labels.text.stroke','stylers':[{'color':'#74001b'},{'lightness':-8}]},{'featureType':'transit','elementType':'geometry','stylers':[{'color':'#6a0d10'},{'visibility':'on'}]},{'featureType':'administrative','elementType':'geometry','stylers':[{'color':'#ffdfa6'},{'weight':0.4}]},{'featureType':'road.local','elementType':'geometry.stroke','stylers':[{'visibility':'off'}]}],
+    mapOptions     = {center: new google.maps.LatLng(lat, lng), zoom: zoom, mapTypeId: google.maps.MapTypeId.HYBRID, styles:styles};
+    map            = new google.maps.Map(document.getElementById('map'), mapOptions);
   }
 })();
