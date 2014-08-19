6feff88eb2e70d5d3b6e37436d5cd01d4151c58a
Logan Richardson <g.logan.richardson@gmail.com>
2014-08-18 23:27:12 -0500

    added photo upload and download functionality

--- app/models/vacation.js
+++ app/models/vacation.js
@@ -1,10 +1,17 @@
 'use strict';
 
+var Mongo = require('mongodb'),
+    cp    = require('child_process'),
+    fs    = require('fs'),
+    path  = require('path'),
+    _     = require('lodash');
+
 function Vacation(o){
   this.name        = o.name;
   this.coordinates = {lat:parseFloat(o.lat), lng:parseFloat(o.lng)};
   this.start       = new Date(o.start);
   this.end         = new Date(o.end);
+  this.photos      = [];
 }
 
 Object.defineProperty(Vacation, 'collection', {
@@ -20,4 +27,47 @@ Vacation.all = function(cb){
   Vacation.collection.find().toArray(cb);
 };
 
+Vacation.findById = function(id, cb){
+  var _id = Mongo.ObjectID(id);
+  Vacation.collection.findOne({_id:_id}, function(err, obj){
+    cb(err, _.create(Vacation.prototype, obj));
+  });
+};
+
+//The Node way to upload files//
+Vacation.prototype.uploadPhoto = function(files, cb){
+  var dir   = __dirname + '/../static/img/' + this._id,
+      exits = fs.existsSync(dir),
+      self  = this;
+
+  if(!exits){
+    fs.mkdirSync(dir);
+  }
+  //This is how we do a shortened for loop//
+  files.photos.forEach(function(photo){
+    var ext    = path.extname(photo.path),
+        rel = '/img/' + self._id + '/' + self.photos.length + ext,
+        abs = dir + '/' + self.photos.length + ext;
+    fs.renameSync(photo.path, abs);
+    self.photos.push(rel);
+  });
+  Vacation.collection.save(self, cb);
+};
+
+Vacation.prototype.downloadPhoto = function(url, cb){
+  var extensions = url.split('.'),
+      extension = extensions[extensions.length -1],
+      dir       = this._id,
+      file      = this.photos.length + '.' + extension,
+      self      = this;
+  cp.execFile(__dirname + '/../scripts/download.sh', [url, file, dir], {cwd:__dirname + '/../scripts'}, function(err, stdout, stderr){
+    var photo = '/img/' + dir + '/' + file;
+    self.photos.push(photo);
+    Vacation.collection.save(self, cb);
+  });
+};
+
 module.exports = Vacation;
+
+
+
